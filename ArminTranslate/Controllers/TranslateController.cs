b7f7980a14e7a97a2664eb750bf3a8d556179d2d
armin habibi <workwitharmin7@gmail.com>
2020-10-05 16:08:16 +0330

    Add project files.

---
+++ ArminTranslate/Controllers/TranslateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace ArminTranslate.Controllers
+{
+    public class TranslateController : Controller
+    {
+        public IActionResult Translate(string from,string to,string text)
+        {
+            var client = new RestClient($"https://api.codebazan.ir/lang/google/?FROM=fa&TO=en&TEXT={text}");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            ViewBag.Content = response.Content;
+
+            return View();
+        }
+    }
+}
---
+++ ArminTranslate/Startup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ArminTranslate
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseMvc(opt =>
+            {
+                opt.MapRoute(
+                    name: "Defaults",
+                    template: "{controller=Translate}/{action=Translate}/{id?}"
+                    );
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
