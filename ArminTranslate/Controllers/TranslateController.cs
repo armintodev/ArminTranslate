8c3d3626e2f4a57214f3426983f780ad5c42d5dd
arminatwork <workwitharmin7@gmail.com>
2021-04-26 17:03:55 +0430

    refactor clean coding

--- ArminTranslate/Controllers/TranslateController.cs
+++ ArminTranslate/Controllers/TranslateController.cs
@@ -1,24 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RestSharp;
 
 namespace ArminTranslate.Controllers
 {
     public class TranslateController : Controller
     {
-        public IActionResult Translate(string from,string to,string text)
+        [HttpGet]
+        public IActionResult Translate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Translate(string from, string to, string text)
         {
             //translate api
             var client = new RestClient($"https://api.codebazan.ir/lang/google/?FROM={from}&TO={to}&TEXT={text}");
-            
+
             //request api
             var request = new RestRequest(Method.GET);
-            
+
             //response api
             IRestResponse response = client.Execute(request);
+            if (!response.IsSuccessful)
+            {
+                return new JsonResult("خطایی در سمت سرویس رخ داده است");
+            }
 
             //this is for return value to view .
             ViewBag.Content = response.Content;
