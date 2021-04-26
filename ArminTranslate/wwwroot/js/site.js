25818db93ca9b765b3d24faac25ec50de74d1827
arminatwork <workwitharmin7@gmail.com>
2021-04-26 17:03:14 +0430

    feat added jquery template

---
+++ ArminTranslate/wwwroot/js/site.js
@@ -0,0 +1,122 @@
+﻿var SinglePage = {};
+
+$(document).ready(function () {
+    $(document).on("submit",
+        'form[data-ajax="true"]',
+        function (e) {
+            e.preventDefault();
+            var form = $(this);
+            const method = form.attr("method").toLocaleLowerCase();
+            const url = form.attr("action");
+            var action = form.attr("data-action");
+
+            if (method === "get") {
+                const data = form.serializeArray();
+                $.get(url,
+                    data,
+                    function (data) {
+                        CallBackHandler(data, action, form);
+                    });
+            } else {
+                var formData = new FormData(this);
+                $.ajax({
+                    url: url,
+                    type: "post",
+                    data: formData,
+                    enctype: "multipart/form-data",
+                    dataType: "json",
+                    processData: false,
+                    contentType: false,
+                    success: function (data) {
+                        CallBackHandler(data, action, form);
+                    },
+                    error: function (data) {
+                        alert("خطایی رخ داده است. لطفا با مدیر سیستم تماس بگیرید.");
+                    }
+                });
+            }
+            return false;
+        });
+});
+
+function CallBackHandler(data, action, form) {
+    switch (action) {
+        case "Message":
+            alert(data.Message);
+            break;
+        case "Refresh":
+            if (data.isSuccedded) {
+                window.location.reload();
+            } else {
+                alert(data.message);
+            }
+            break;
+        case "RefereshList":
+            {
+                //hideModal();
+                const refereshUrl = form.attr("data-refereshurl");
+                const refereshDiv = form.attr("data-refereshdiv");
+                get(refereshUrl, refereshDiv);
+            }
+            break;
+        case "setValue":
+            {
+                const element = form.data("element");
+                $(`#${element}`).html(data);
+            }
+            break;
+        default:
+    }
+}
+
+function get(url, refereshDiv) {
+    const searchModel = window.location.search;
+    $.get(url,
+        searchModel,
+        function (result) {
+            $("#" + refereshDiv).html(result);
+        });
+}
+
+
+function fillField(source, dist) {
+    const value = $('#' + source).val();
+    $('#' + dist).val(value);
+}
+
+$(document).on("click",
+    'button[data-ajax="true"]',
+    function () {
+        const button = $(this);
+        const form = button.data("request-form");
+        const data = $(`#${form}`).serialize();
+        let url = button.data("request-url");
+        const method = button.data("request-method");
+        const field = button.data("request-field-id");
+        if (field !== undefined) {
+            const fieldValue = $(`#${field}`).val();
+            url = url + "/" + fieldValue;
+        }
+        if (button.data("request-confirm") == true) {
+            if (confirm("آیا از انجام این عملیات اطمینان دارید؟")) {
+                handleAjaxCall(method, url, data);
+            }
+        } else {
+            handleAjaxCall(method, url, data);
+        }
+    });
+
+function handleAjaxCall(method, url, data) {
+    if (method === "post") {
+        $.post(url,
+            data,
+            "application/json; charset=utf-8",
+            "json",
+            function (data) {
+
+            }).fail(function (error) {
+                alert("خطایی رخ داده است. لطفا با مدیر سیستم تماس بگیرید.");
+            });
+    }
+}
+
